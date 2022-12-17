30e333b7eb3206752e5126345334f13d014973e7
bruins288 <bruins288@gmail.com>
2022-12-17 20:50:14 +0300

    Создан отдельный компонент Drawer

--- src/App.js
+++ src/App.js
@@ -2,44 +2,13 @@ import React from "react";
 
 import Card from "./components/Card.jsx";
 import Header from "./components/Header.jsx";
+import Drawer from "./components/Drawer.jsx";
 
 function App() {
   return (
     <div className="wrapper">
       <aside className="overlay" style={{ display: "none" }}>
-        <div className="overlay__drawer">
-          <h2>
-            Корзина <img src="/img/delete.svg" alt="Remove" />
-          </h2>
-          <div className="cart">
-            <div className="cart__item">
-              <img src="/img/sneaks/1.jpg" alt="sneak" width={70} height={70} />
-              <div className="cart__item__info">
-                <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-                <strong>12 999 руб.</strong>
-              </div>
-              <img src="/img/delete.svg" alt="Remove" />
-            </div>
-          </div>
-          <div className="cart__summer">
-            <ul>
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <strong>21 498 руб.</strong>
-              </li>
-              <li>
-                <span>Налог 5%:</span>
-                <div></div>
-                <strong>1074 руб.</strong>
-              </li>
-            </ul>
-            <button>
-              Оформить заказ
-              <img src="/img/arrow.svg" alt="arrow" />
-            </button>
-          </div>
-        </div>
+        <Drawer />
       </aside>
       <Header />
       <main className="content">
